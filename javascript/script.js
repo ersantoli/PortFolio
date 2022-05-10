00e4e58ecb1d7059f7ae5ca2b0a1ab7c689f7222
ersantoli <74686375+ersantoli@users.noreply.github.com>
2022-05-09 23:40:53 -0300

    add missing javascript files

---
+++ javascript/script.js
@@ -0,0 +1,42 @@
+var projetos = {
+    crud:"Crud basico com  javascript",
+    texto2:"Campanha do Agasalho",Image,
+    texto3:"outra coisa",
+}
+
+var descricao = document.getElementById('descricao');
+var detalhes = document.getElementById('detalhes');
+var iframe= document.getElementById('ifr');
+
+
+function preencheDescricao() {
+
+    detalhes.innerHTML= camisa.texto;
+    
+     wrs.src="../imagem/Agasalho.png";   
+    
+    
+}
+
+    
+function preencheDescricao1() {
+    detalhes.innerHTML= projetos.crud;
+   
+  iframe.src="../Projetos/crud_responsivo.html";
+}
+
+function preencheDescricao2() {
+    detalhes.innerHTML= camisa.texto3;
+   
+    
+}
+function TrocaImagem(){
+    fts_Perfil.src="../imagem/perfil_cor.jpg";
+    
+        
+    
+}
+function ImagemOriginal(){
+    fts_Perfil.src="../imagem/Imagem_Perfil.jpg"
+
+}
